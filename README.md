983ca38c4d1c2568280f78ea91a58c842be7bd43
Morten Vassvik <mvassvik@gmail.com>
2017-11-10 01:07:58 +0100

    added files

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # odin-stb
 
 
+cl -nologo -MT -TC -c stb_image.c
+lib -nologo stb_image.obj -out:stb_image.lib
+lib -nologo stb_image_write.obj -out:stb_image_write.lib
+lib -nologo stb_truetype.obj -out:stb_truetype.lib
---
+++ src/build.bat
@@ -0,0 +1,7 @@
+
+cl -nologo -MT -TC -O2 -c stb_image.c stb_image_write.c stb_truetype.c
+lib -nologo stb_image.obj -out:..\lib\stb_image.lib
+lib -nologo stb_image_write.obj -out:..\lib\stb_image_write.lib
+lib -nologo stb_truetype.obj -out:..\lib\stb_truetype.lib
+
+del stb_image.obj stb_image_write.obj stb_truetype.obj
