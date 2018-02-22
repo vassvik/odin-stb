b2d1cce7c492f9c4d1b015f9b0d8ad14f0855275
thebirk <pingnor@gmail.com>
2018-02-22 13:48:17 +0100

    Updated build.bat.

--- src/build.bat
+++ src/build.bat
@@ -1,6 +1,6 @@
 if not exist "..\lib" mkdir ..\lib
 
-cl -nologo -MT -TC -O2 -c stb_image.c stb_image_write.c stb_truetype.c stb_rect_pack.h
+cl -nologo -MT -TC -O2 -c stb_image.c stb_image_write.c stb_truetype.c stb_rect_pack.c
 lib -nologo stb_image.obj -out:..\lib\stb_image.lib
 lib -nologo stb_image_write.obj -out:..\lib\stb_image_write.lib
 lib -nologo stb_truetype.obj -out:..\lib\stb_truetype.lib
