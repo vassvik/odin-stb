09b6b986fb7593385d10ba63f82253edf4b9f355
thebirk <pingnor@gmail.com>
2018-02-22 13:46:34 +0100

    Untested stb_rect_pack binding.

--- src/build.bat
+++ src/build.bat
@@ -1,8 +1,9 @@
 if not exist "..\lib" mkdir ..\lib
 
-cl -nologo -MT -TC -O2 -c stb_image.c stb_image_write.c stb_truetype.c
+cl -nologo -MT -TC -O2 -c stb_image.c stb_image_write.c stb_truetype.c stb_rect_pack.h
 lib -nologo stb_image.obj -out:..\lib\stb_image.lib
 lib -nologo stb_image_write.obj -out:..\lib\stb_image_write.lib
 lib -nologo stb_truetype.obj -out:..\lib\stb_truetype.lib
+lib -nologo stb_rect_pack.obj -out:..\lib\stb_rect_pack.lib
 
-del stb_image.obj stb_image_write.obj stb_truetype.obj
+del stb_image.obj stb_image_write.obj stb_truetype.obj stb_rect_pack.obj
